031f75bd1126b1721d4684969b8be7763daedc08
snoopyk921 <snoopyk921@gmail.com>
2012-11-17 19:42:36 +1000

    First commit, initial pre-built setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DualitySoftware::Application.config.secret_token = '9d94e8024bb9bf7a9a7b47bd6fb78da8310442e22e9697bbd8ecaefc6d4c6e86cd2130c460094275f30826b804aea88899ffe6ee596ee1813eb944648bfe93bf'
