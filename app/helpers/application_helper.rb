38bf1c54c03626ea12d178f6d6b984dfe74c4510
snoopyk921 <snoopyk921@gmail.com>
2012-11-17 19:48:13 +1000

    Added dynamic title helper and tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #return full title plus current page, should look like "Duality Software - Page"
+  def full_title(page_title)
+    base_title = "Duality Software"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} - #{page_title}"
+    end
+  end
 end
